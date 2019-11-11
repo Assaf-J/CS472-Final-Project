d58448955190c5596060da5bec7b831bf542f4c5
Assafj-OregonState <38144145+Assafj-OregonState@users.noreply.github.com>
2019-11-10 16:01:56 -0800

    Add Edge detection filter to load image.

--- Doodle-Physics/Assets/LoadingImage.cs
+++ Doodle-Physics/Assets/LoadingImage.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
-using UnityEngine.SceneManagement;
+
 public class LoadingImage : MonoBehaviour
 {
     public static string fileName;
     Texture2D loadedImageTexture;
     // Start is called before the first frame update
+    public float th = 0.09f;
+    System.Diagnostics.Stopwatch s;
+    static Texture2D img, rImg, gImg, bImg, kImg, roImg, goImg, boImg, koImg, SUMimg;
+    static float[,] rL, gL, bL, kL, SUML;
+    static Color temp;
+    static float t;
     void Start()
     {
         fileName = CapturePhoto.fileName;
@@ -17,16 +23,147 @@ public class LoadingImage : MonoBehaviour
 
         loadedImageTexture = new Texture2D(300, 200, TextureFormat.RGB24, false);
         loadedImageTexture.LoadImage(readImage);
-        GameObject newImage = GameObject.Find("Background");     //newImage is the backGround from webcam now
-        newImage.GetComponent<RawImage>().texture = loadedImageTexture;    //Sets
+       // GameObject newImage = GameObject.Find("Background");     //newImage is the backGround from webcam now
+       // newImage.GetComponent<RawImage>().texture = loadedImageTexture;
+
+        img = new Texture2D(loadedImageTexture.width, loadedImageTexture.height);
+        //initialize textures
+        rImg = new Texture2D(img.width, img.height);
+        bImg = new Texture2D(img.width, img.height);
+        gImg = new Texture2D(img.width, img.height);
+        kImg = new Texture2D(img.width, img.height);
+        //initialize gradient arrays
+        rL = new float[img.width, img.height];
+        gL = new float[img.width, img.height];
+        bL = new float[img.width, img.height];
+        kL = new float[img.width, img.height];
+       
+        SUML = new float[img.width, img.height];
+       
+        roImg = new Texture2D(img.width, img.height);
+        goImg = new Texture2D(img.width, img.height);
+        boImg = new Texture2D(img.width, img.height);
+        koImg = new Texture2D(img.width, img.height);
+       
+        SUMimg = new Texture2D(img.width, img.height);
+     
+
+
+        GetWebCamImage();
 
     }
+    void GetWebCamImage()
+    {
+        img.SetPixels(loadedImageTexture.GetPixels());
+        CalculateEdges();
+    }
 
+    void CalculateEdges()
+    {
+
+        //calculate new textures
+        for (int x = 0; x < img.width; x++)
+        {
+            for (int y = 0; y < img.height; y++)
+            {
+                temp = img.GetPixel(x, y);
+                rImg.SetPixel(x, y, new Color(temp.r, 0, 0));
+                gImg.SetPixel(x, y, new Color(0, temp.g, 0));
+                bImg.SetPixel(x, y, new Color(0, 0, temp.b));
+                t = temp.r + temp.g + temp.b;
+                t /= 3f;
+                kImg.SetPixel(x, y, new Color(t, t, t));
+            }
+        }
+        rImg.Apply();
+        gImg.Apply();
+        bImg.Apply();
+        kImg.Apply();
+
+        //calculate gradient values
+        for (int x = 0; x < img.width; x++)
+        {
+            for (int y = 0; y < img.height; y++)
+            {
+                rL[x, y] = gradientValue(x, y, 0, rImg);
+                gL[x, y] = gradientValue(x, y, 1, gImg);
+                bL[x, y] = gradientValue(x, y, 2, bImg);
+                kL[x, y] = gradientValue(x, y, 2, kImg);
+              
+                SUML[x, y] = rL[x, y] + gL[x, y] + bL[x, y];
+                
+            }
+        }
+        //create texture from gradient values
+        TextureFromGradientRef(rL, th, ref roImg);
+        TextureFromGradientRef(gL, th, ref goImg);
+        TextureFromGradientRef(bL, th, ref boImg);
+        TextureFromGradientRef(kL, th, ref koImg);
+    
+        TextureFromGradientRef(SUML, th, ref SUMimg);
+  
+    }
 
     // Update is called once per frame
-
-    void Update()
+    void OnGUI()
     {
+        GUILayout.BeginHorizontal();
+        GUILayout.BeginVertical();
+        GUILayout.Label("SUM detection");
+        GUILayout.Label(SUMimg);
+        GUILayout.EndVertical();
+
+        GUILayout.EndHorizontal();
+    }
+
+    float gradientValue(int ex, int why, int colorVal, Texture2D image)
+    {
+        float lx = 0f;
+        float ly = 0f;
+        if (ex > 0 && ex < image.width)
+            lx = 0.5f * (image.GetPixel(ex + 1, why)[colorVal] - image.GetPixel(ex - 1, why)[colorVal]);
+        if (why > 0 && why < image.height)
+            ly = 0.5f * (image.GetPixel(ex, why + 1)[colorVal] - image.GetPixel(ex, why - 1)[colorVal]);
+        return Mathf.Sqrt(lx * lx + ly * ly);
+    }
+
+    Texture2D TextureFromGradient(float[,] g, float thres)
+    {
+        Texture2D output = new Texture2D(g.GetLength(0), g.GetLength(1));
+        for (int x = 0; x < output.width; x++)
+        {
+            for (int y = 0; y < output.height; y++)
+            {
+                if (g[x, y] >= thres)
+                    output.SetPixel(x, y, Color.black);
+                else
+                    output.SetPixel(x, y, Color.white);
+            }
+        }
+        output.Apply();
+        
+        return output;
+    }
+
+    void TextureFromGradientRef(float[,] g, float thres, ref Texture2D output)
+    {
+        for (int x = 0; x < output.width; x++)
+        {
+            for (int y = 0; y < output.height; y++)
+            {
+                if (g[x, y] >= thres)
+                    output.SetPixel(x, y, Color.black);
+                else
+                    output.SetPixel(x, y, Color.white);
+            }
+        }
+        output.Apply();
         
     }
+    void Update()
+    {
+    }
 }
+
+
+
