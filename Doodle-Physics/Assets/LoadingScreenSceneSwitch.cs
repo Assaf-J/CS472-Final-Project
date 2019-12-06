bb35f931d8d8244ee44a9033977abade723ebabb
Jarrell <nelsojar@oregonstate.edu>
2019-12-05 22:15:26 -0800

    Added Loading Animation
    Added loading animation

---
+++ Doodle-Physics/Assets/LoadingScreenSceneSwitch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreenSceneSwitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          StartCoroutine(LoadGamePlayScene());
+    }
+
+     IEnumerator LoadGamePlayScene()
+     {
+          AsyncOperation async = SceneManager.LoadSceneAsync("load_photo");
+          yield return new WaitForEndOfFrame();
+
+          //while(!async.isDone)
+          //{
+               //yield return new WaitForEndOfFrame();
+          //}
+     }
+}
